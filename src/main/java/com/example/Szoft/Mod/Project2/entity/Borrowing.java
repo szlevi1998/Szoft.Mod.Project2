809806019bd1917aae2f6a3b13a34b8cda9a2423
udvardymarton <martonudvardy4@gmail.com>
2020-12-03 17:57:50 +0100

    Project: Borrowing entity hozzáadása.

--- src/main/java/com/example/Szoft/Mod/Project2/entity/Borrowing.java
+++ src/main/java/com/example/Szoft/Mod/Project2/entity/Borrowing.java
@@ -1,4 +1,47 @@
 package com.example.Szoft.Mod.Project2.entity;
 
+import com.example.Szoft.Mod.Project2.views.Views;
+import com.fasterxml.jackson.annotation.JsonView;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "kolcsonzes")
 public class Borrowing {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "BORROWING_ID")
+    @JsonView(Views.Base.class)
+    private int id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "PERSON_ID")
+    @JsonView(Views.FromMovie.class)
+    private Person fk_person;
+
+    public Borrowing(int id, Person fk_person){
+        this.id = id;
+        this.fk_person = fk_person;
+    }
+
+    public Borrowing(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Person getFk_person() {
+        return fk_person;
+    }
+
+    public void setFk_person(Person fk_person) {
+        this.fk_person = fk_person;
+    }
 }
