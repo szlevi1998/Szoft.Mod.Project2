ddaeef1e54da33eed7bc4ed9c724838351bea2d0
szlevi1998 <sz.levi1998@gmail.com>
2020-12-03 20:33:55 +0100

    UI és backend összekötetés(3).

--- src/main/java/com/example/Szoft/Mod/Project2/Application.java
+++ src/main/java/com/example/Szoft/Mod/Project2/Application.java
@@ -2,12 +2,32 @@ package com.example.Szoft.Mod.Project2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.Collections;
+
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Collections.singletonList("https://szlevi1998.github.io/Szoft.Mod.Project2/demo/ava_movies.html"));
+                config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 
 }
